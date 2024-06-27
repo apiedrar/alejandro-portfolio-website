ca026d18006ca22f4db7b24a72ce56c02c050712
Alejandro Piedra Rios <159655906+apiedrar@users.noreply.github.com>
2024-06-27 13:46:42 -0600

    finish nav bar

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { Bars3Icon } from "@heroicons/react/16/solid";
+import { XMarkIcon } from "@heroicons/react/16/solid";
 import { useState } from "react";
 
 export default function Navbar() {
@@ -8,24 +10,41 @@ export default function Navbar() {
     }
     
     return(
-        <nav className="fixed w-full h-24 mb-100px">
+        <nav className="fixed w-screen h-24 mb-100px" style={{backgroundColor: "rgba(214, 219, 220, 0.95)"}}>
         <div className="flex justify-between items-center h-full w-full px-4">
           <Link href="https://www.linkedin.com/in/apiedrar/">
-            <h1 className="text-2xl lg:text-5xl inline">
+            <h1 className="text-lg lg:text-5xl inline">
               Alejandro Piedra Rios
             </h1>
           </Link>
-          <div>
-            <ul className="hidden sm:flex inline">
+          <div className="hidden sm:flex">
+            <ul className="hidden sm:flex">
               <Link href="https://www.x.com/apiedrar/">
-                <li className="text-xl">About Me</li>
+                <li className="ml-5 hover:border p-4 text-xl">About Me</li>
               </Link>
               <Link href="https://www.github.com/apiedrar/apr-portfolio-web">
-                <li className=" text-xl">Check on GitHub</li>
+                <li className="mx-5 hover:border p-4 text-xl">Check on GitHub</li>
               </Link>
             </ul>
           </div>
-          <div>
+          <div onClick={handleNav} className="sm:hidden cursor-pointer pl-24"><Bars3Icon className="size-7 text-gray-500" />
+          </div>
+          <div className={menuOpen ? "fixed top-0 left-0 w-screen sm:hidden h-[28%] bg-gray-300 p-10 ease-in duration-500" : "fixed top-[-100%] left-0 p-10 ease-in duration-500"}>
+            <div className="flex w-full items-center justify-end">
+              <div onClick={handleNav} className="cursor-pointer">
+                <XMarkIcon className="size-7 text-gray-500"/>
+              </div>
+            </div>
+            <div className="flex-col py-4">
+                <ul>
+              <Link href="https://www.x.com/apiedrar/">
+                <li className="py-4 cursor-pointer">About Me</li>
+              </Link>
+              <Link href="https://www.github.com/apiedrar/apr-portfolio-web">
+                <li className="py-4 cursor-pointer">Check on GitHub</li>
+                </Link>
+                </ul>
+            </div>
           </div>
         </div>
       </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Navbar from "./Navbar.jsx";
 
@@ -5,10 +6,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-stretch">
       <Navbar />
-      <div className="w-full justify-center font-mono lg:flex mt-40 mb-12">
-        <h2 className="text-xl lg:text-4xl">
-          Welcome to my Porfolio Landing Page!
-        </h2>
+      <div className="max-w-[240px] justify-center font-mono mt-40 mb-12 lg:flex lg:max-w-full">
+        <h2 className="text-lg lg:text-4xl">Welcome to my Landing Page!</h2>
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] lg:mt-8 mb-4">
@@ -30,20 +29,20 @@ export default function Home() {
 
       <div className="mb-5 grid text-center lg:mb-0 lg:w-full lg:max-w-7xl lg:grid-cols-5 lg:text-left lg:place-items-stretch">
         <a
-          href="/calculadora-roi"
+          href="/calculadora-roi/calculadora-roi"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="/calculadora-roi"
           rel=""
         >
           <h2 className="mb-2 text-2xl font-semibold">
-            ROI Calculator{" "}
+            Spanish ROI Calculator{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="mt-2 max-w-[30ch] text-sm opacity-50">
-            A simple and intuitive Calculator that shows the power of compound
-            interest over time with a graphic! En Español.
+          <p className="mt-2 max-w-[30ch] text-sm">
+            Compound Interest Calculator, with a Graphic and built using Next.js
+            and Primereact.
           </p>
         </a>
 
@@ -59,8 +58,9 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className="mt-2 max-w-[30ch] text-sm opacity-50">
-            A beautiful user-friendly percentage calculator!
+          <p className="mt-2 max-w-[30ch] text-sm">
+            Beautiful, user-friendly percentage calculator! Built emulating MVC
+            Architecture.
           </p>
         </a>
 
@@ -71,13 +71,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-2 text-2xl font-semibold">
-            Mockddit{" "}
+            Mockddit Phone Home{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="mt-2 max-w-[30ch] text-sm opacity-50">
-            A functional mockup of Reddit!
+          <p className="mt-2 max-w-[30ch] text-sm">
+            Built using React and Redux, and integrating real data from Reddit's
+            API.
           </p>
         </a>
 
@@ -88,14 +89,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-2 text-2xl font-semibold">
-            Toggipon{" "}
+            Duel Against the Machine{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="mt-2 max-w-[30ch] text-sm opacity-50">
-            A simple game based off a beloved franchise, for showcasing purposes
-            only.
+          <p className="mt-2 max-w-[30ch] text-sm">
+            A simple game based off of a beloved franchise! Built in old
+            fashioned HTML, CSS and JavaScript.
           </p>
         </a>
 
@@ -106,12 +107,12 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-2 text-2xl font-semibold">
-            My top Indie Project TD{" "}
+            Top Indie Project TD{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="mt-2 max-w-[30ch] text-balance text-sm opacity-50">
+          <p className="mt-2 max-w-[30ch] text-balance text-sm">
             Take a look at the latest collaboration I'm most proud of (To Date).
           </p>
         </a>
