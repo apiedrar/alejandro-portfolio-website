45142d37a8c19b0ae6024d5364e85a50dd26d34f
Alejandro Piedra Rios <159655906+apiedrar@users.noreply.github.com>
2024-07-19 21:02:08 -0600

    merge and continue from here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       <div className="mb-5 grid text-center lg:mb-0 lg:w-full lg:max-w-7xl lg:grid-cols-5 lg:text-left lg:place-items-stretch">
         <Link
           href="/roi-calculator"
-          className="hidden group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
           <h2 className="mb-2 text-2xl font-semibold">
