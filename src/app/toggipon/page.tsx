b3ec38cba0785219e76c9754504adac410350e61
Alejandro Piedra Rios <159655906+apiedrar@users.noreply.github.com>
2024-08-25 19:59:03 -0600

    work background image

---
+++ src/app/toggipon/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Navbar from "@/app/Navbar.jsx";
+import { useState } from "react";
+import "./Toggipon.css";
+
+export default function Toggipon() {
+  const [game, setGame] = useState(["Player", "Machine"]);
+
+  return <main></main>;
+}
