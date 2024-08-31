05286e608985aad9e39bc2f1aa5a09da2d296bbc
Alejandro Piedra Rios <159655906+apiedrar@users.noreply.github.com>
2024-08-30 21:03:29 -0600

    progress labels layout

--- src/app/toggipon/Pet.jsx
+++ src/app/toggipon/Pet.jsx
@@ -1,44 +1,13 @@
 import Image from "next/image";
 
-export default function Pet({playersPet, setPlayersPet}) {
+export default function Pet({playersPet, setPlayersPet, petName, petTag}) {
     return (
-        <div>
-            <input className="hidden" type="radio" id="hipodoge" value={"Hipodoge"} onChange={(e) => setPlayersPet(e.value)} checked={playersPet === "Hipodoge"}>
-                <label htmlFor="hipodoge">
-                Hipodoge
-                <Image className="w-[20px]" src="/Hipodoge.png" alt="Hipodoge" />
-                </label>
-            </input>
-            <input className="hidden" type="radio" id="ratigueya" value={"Ratigueya"} onChange={(e) => setPlayersPet(e.value)} checked={playersPet === "Ratigueya"}>
-                <label htmlFor="ratigueya">
-                Ratigueya
-                <Image className="w-[20px]" src="/Ratigueya.png" alt="Ratigueya" />
-                </label>
-            </input>
-            <input className="hidden" type="radio" id="capipepo" value={"Capipepo"} onChange={(e) => setPlayersPet(e.value)} checked={playersPet === "Capipepo"}>
-                <label htmlFor="capipepo">
-                Capipepo
-                <Image className="w-[20px]" src="/Capipepo.png" alt="Capipepo" />
-                </label>
-            </input>
-            <input className="hidden" type="radio" id="tucapalma" value={"Tucapalma"} onChange={(e) => setPlayersPet(e.value)} checked={playersPet === "Tucapalma"}>
-                <label htmlFor="tucapalma">
-                Tucapalma
-                <Image className="w-[20px]" src="/Tucapalma.png" alt="Tucapalma" />
-                </label>
-            </input>
-            <input className="hidden" type="radio" id="langostelvis" value={"Langostelvis"} onChange={(e) => setPlayersPet(e.value)} checked={playersPet === "Langostelvis"}>
-                <label htmlFor="langostelvis">
-                Langostelvis
-                <Image className="w-[20px]" src="/Langostelvis.png" alt="Langostelvis" />
-                </label>
-            </input>
-            <input className="hidden" type="radio" id="pydos" value={"Pydos"} onChange={(e) => setPlayersPet(e.value)} checked={playersPet === "Pydos"}>
-                <label htmlFor="pydos">
-                Pydos
-                <Image className="w-[20px]" src="/Pydos.png" alt="Pydos" />
-                </label>
-            </input>
+        <div className="my-[5%] mx-[5%] border rounded-full p-[10%]">
+            <input className="hidden" type="radio" id={petTag} value={petName} onChange={(e) => setPlayersPet(e.value)} checked={playersPet === petName} />
+            <label className="inline-flex" htmlFor={petTag}>
+            {petName}
+            <Image className="w-[35%] ml-[10%]" src={`/${petName}.png`} alt={petName} width={150} height={150} />
+            </label>
         </div>
     )
 }
--- src/app/toggipon/page.tsx
+++ src/app/toggipon/page.tsx
@@ -9,15 +9,51 @@ export default function Toggipon() {
   const [playersPet, setPlayersPet] = useState("Hipodoge");
 
   return (
-    <main>
-      <section className="grid-cols-2 grid-rows-3">
-        <Pet playersPet={playersPet} setPlayersPet={setPlayersPet} />
-        <Pet playersPet={playersPet} setPlayersPet={setPlayersPet} />
-        <Pet playersPet={playersPet} setPlayersPet={setPlayersPet} />
-        <Pet playersPet={playersPet} setPlayersPet={setPlayersPet} />
-        <Pet playersPet={playersPet} setPlayersPet={setPlayersPet} />
-        <Pet playersPet={playersPet} setPlayersPet={setPlayersPet} />
-      </section>
+    <main className="flex flex-col justify-center align-center">
+      <h1 className="self-center my-[5%]">T O G G I P O N !</h1>
+      <div className="flex justify-center align-center">
+        <section className="flex flex-col justify-center align-center">
+          <h2 className="my-[10%] self-center">Choose your Pet:</h2>
+          <div className="grid grid-cols-1 grid-rows-6 md:grid-cols-2 md:grid-rows-3">
+            <Pet
+              playersPet={playersPet}
+              setPlayersPet={setPlayersPet}
+              petName="Hipodoge"
+              petTag="hipodoge"
+            />
+            <Pet
+              playersPet={playersPet}
+              setPlayersPet={setPlayersPet}
+              petName="Capipepo"
+              petTag="capipepo"
+            />
+            <Pet
+              playersPet={playersPet}
+              setPlayersPet={setPlayersPet}
+              petName="Ratigueya"
+              petTag="ratigueya"
+            />
+            <Pet
+              playersPet={playersPet}
+              setPlayersPet={setPlayersPet}
+              petName="Langostelvis"
+              petTag="langostelvis"
+            />
+            <Pet
+              playersPet={playersPet}
+              setPlayersPet={setPlayersPet}
+              petName="Tucapalma"
+              petTag="tucapalma"
+            />
+            <Pet
+              playersPet={playersPet}
+              setPlayersPet={setPlayersPet}
+              petName="Pydos"
+              petTag="pydos"
+            />
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
