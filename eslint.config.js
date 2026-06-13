7bd523a5a469d755e3f116b521c4dff042be5c57
Alejandro Piedra Rios <159655906+apiedrar@users.noreply.github.com>
2026-06-12 21:22:36 -0600

    update package manager, fix linting error

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,7 @@
-export default {
-  extends: ["next/core-web-vitals"],
-};
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextCoreWebVitals,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
